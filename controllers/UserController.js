cf48afdf099f3b47eb8454dd03c2182d51b9f038
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-04-02 13:09:53 +0530

    before start angular

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,14 +1,64 @@
 const express = require('express');
 var router = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
 
-var { User } = require('../models/employee');
+var { User } = require('../models/user');
 
 // => localhost:3000/users/
 router.get('/', (req, res) => {
   User.find((err, docs) => {
     if (!err) { res.send(docs);}
-    else { console.log('Error in Retriving Users :' + JSON.stringify(err, undefined, 2))}
+    else { console.log('Error in Retriving Users :' + JSON.stringify(err, undefined, 2)); }
   });
 });
 
-module.exports = router;
+// get id's => localhost:3000/users/id
+router.get('/:id', (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+  User.findById(req.params.id, (err, doc) => {
+    if (!err) { res.send(doc);}
+    else { console.log('Error in Retriving Users :' + JSON.stringify(err, undefined, 2)); }
+  });
+});
+
+router.post('/', (req, res) => {
+  var cuser = new User ({
+    name: req.body.name,
+    equipment: req.body.equipment,
+    time: req.body.time, 
+    damage: req.body.damage
+  });
+  cuser.save((err, doc) => {
+    if (!err) { res.send(doc); }
+    else { console.log('Error in User Save :' + JSON.stringify(err, undefined, 2)); }
+  });
+});
+
+router.put('/:id', (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+  var cuser = ({
+    name: req.body.name,
+    equipment: req.body.equipment,
+    time: req.body.time, 
+    damage: req.body.damage
+  });
+  User.findByIdAndUpdate(req.params.id, { $set: emp }, { new: true }, (err, doc) => {
+    if (!err) { res.send(doc); }
+    else { console.log('Error in User Update :' + JSON.stringify(err, undefined, 2)); }
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+    User.findByIdAndRemove(req.params.id, (err, doc) => {
+      if (!err) { res.send(doc); }
+      else { console.log('Error in User Update :' + JSON.stringify(err, undefined, 2)); }
+  });
+
+module.exports = router;  
--- models/user.js
+++ models/user.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 var User = mongoose.model('User', {
   name: { type: String },
-  position: { type: String},
   equipment: { type: String},
-  time: { type: Number},
-  damage: { type: String},
+  time: { type: Number}, 
+  damage: { type: String}
 });
 
 module.exports = { User };
